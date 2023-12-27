700ad3be35f7fd48d6839e7fe83c53beba4e352c
xpatxj <patrycjagrzesiak100@gmail.com>
2023-12-27 21:18:22 +0100

    added basic dodajGwiazde method

--- src/Gwiazda.java
+++ src/Gwiazda.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Gwiazda {
     private String nazwa;
     private String nazwaKatalogowa;
@@ -28,13 +29,16 @@ public class Gwiazda {
         return nazwa.matches("[A-Z]{3}\\d{4}");
     }
     public boolean sprawdzDeklinacje(String deklinacja, String polkula) {
-        if (polkula.equals("polnocna")) {
+        if (polkula.equals("PN")) {
             return deklinacja.matches("([0-9]|[1-8][0-9]|90) stopni ([0-5][0-9]) minut ([0-5]0-9?) sekund");
-        } else if (polkula.equals("poludniowa")) {
+        } else if (polkula.equals("PD")) {
             return deklinacja.matches("-([0-9]|[1-8][0-9]|90) stopni ([0-5][0-9]) minut ([0-5]0-9?) sekund");
         } else {
             return false;
         }
+    }
+    public boolean sprawdzPolkule(String polkula) {
+        return polkula.equals("PN") || polkula.equals("PD");
     }
     public boolean sprawdzRektascensje(String rektascensja) {
         return rektascensja.matches("([01][0-9]|2[0-3]|24) h ([0-5][0-9]) m ([0-5]0-9?) s");
@@ -49,8 +53,33 @@ public class Gwiazda {
         return masa >= 0.1 && masa <= 50;
     }
     public String dodajGwiazde(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nowa gwiazda powinna zawietać następujące dane:");
+        System.out.println("Nazwa katalogowa, deklinacja, rektascensja, obserwowana wielkość gwiazdowa, odległość, gwiazdozbiór, półkula, temperatura, masa");
+        System.out.println("Podaj nazwę katalogową gwiazdy: ");
+        String nazwaKatalogowa = scanner.nextLine();
         if (sprawdzNazwe(nazwaKatalogowa)) {
-            return "Dodano gwiazdę";
+            System.out.println("Podaj deklinację gwiazdy: ");
+            String deklinacja = scanner.nextLine();
+            System.out.println("Podaj rektascensję gwiazdy: ");
+            String rektascensja = scanner.nextLine();
+            System.out.println("Podaj obserwowaną wielkość gwiazdową: ");
+            double obserwowanaWielkoscGwiazdowa = scanner.nextDouble();
+            System.out.println("Podaj odległość gwiazdy: ");
+            double odleglosc = scanner.nextDouble();
+            System.out.println("Podaj gwiazdozbiór gwiazdy: ");
+            String gwiazdozbior = scanner.nextLine();
+            System.out.println("Podaj półkulę gwiazdy: ");
+            String polkula = scanner.nextLine();
+            System.out.println("Podaj temperaturę gwiazdy: ");
+            double temperatura = scanner.nextDouble();
+            System.out.println("Podaj masę gwiazdy: ");
+            double masa = scanner.nextDouble();
+            if (sprawdzDeklinacje(deklinacja, polkula) && sprawdzPolkule(polkula) && sprawdzRektascensje(rektascensja) && sprawdzWielkoscGwiazdowa(obserwowanaWielkoscGwiazdowa) && sprawdzTempearute(temperatura) && sprawdzMase(masa)) {
+                return "Dodano gwiazdę";
+            } else {
+                return "Nie dodano gwiazdy";
+            }
         } else {
             return "Nie dodano gwiazdy";
         }
