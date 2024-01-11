58a8b30994c5b6c5655976c195a44b1eed4ffd72
EmiliaKostrubała <kostrubala.em@gmail.com>
2024-01-11 21:26:03 +0100

    Checking if object class Gwiazda can be created, bug fixes in Regex and adding methods

--- src/Gwiazda.java
+++ src/Gwiazda.java
@@ -1,30 +1,54 @@
 import java.util.Scanner;
+
 public class Gwiazda {
+
     String nazwa;
     BazaGwiazd.AlfabetGrecki literaGrecka;
     String nazwaKatalogowa;
-    double deklinacja;
-    double rektascensja;
+    String deklinacja;
+    String rektascensja;
     double obserwowanaWielkoscGwiazdowa;
     double absolutnaWielkoscGwiazdowa;
     double odleglosc;
     String gwiazdozbior;
-    String polkula;
+    RodzajPolkuli polkula;
     double temperatura;
     double masa;
 
-    Gwiazda(String nazwa, double deklinacja, double rektascensja, double obserwowanaWielkoscGwiazdowa, double odleglosc, String gwiazdozbior, String polkula, double temperatura, double masa) {
-        this.nazwa = nazwa;
+    Gwiazda(String nazwa, String deklinacja, String rektascensja, double obserwowanaWielkoscGwiazdowa, double odleglosc, String gwiazdozbior, RodzajPolkuli polkula, double temperatura, double masa) throws Exception {
+        if(!sprawdzNazwe(nazwa))
+            throw new Exception("Nieprawidlowa nazwa");
+        else
+            this.nazwa = nazwa;
+
         this.nazwaKatalogowa = "";
-        this.deklinacja = deklinacja;
-        this.rektascensja = rektascensja;
-        this.obserwowanaWielkoscGwiazdowa = obserwowanaWielkoscGwiazdowa;
+        if(!sprawdzDeklinacje(deklinacja,polkula))
+            throw new Exception("Nieprawidlowa deklinacja");
+        else
+            this.deklinacja = deklinacja;
+       if(!sprawdzRektascensje(rektascensja))
+           throw new Exception("Nieprawidlowa rektascensja");
+       else
+           this.rektascensja = rektascensja;
+       if(!sprawdzWielkoscGwiazdowa(obserwowanaWielkoscGwiazdowa))
+           throw new Exception("Nieprawidlowa obserwowana wielkość gwiazdowa");
+       else
+           this.obserwowanaWielkoscGwiazdowa = obserwowanaWielkoscGwiazdowa;
         this.odleglosc = odleglosc;
         this.absolutnaWielkoscGwiazdowa = obserwowanaWielkoscGwiazdowa - 5 * Math.log10(odleglosc/3.26) + 5;
         this.gwiazdozbior = gwiazdozbior;
-        this.polkula = polkula;
-        this.temperatura = temperatura;
-        this.masa = masa;
+        if(!sprawdzPolkule(polkula))
+            throw new Exception("Nieprawidłowa nazwa półkuli");
+        else
+            this.polkula = polkula;
+        if(!sprawdzTempearute(temperatura))
+            throw new Exception("Nieprawidłowa temperatura");
+        else
+            this.temperatura = temperatura;
+        if(!sprawdzMase(masa))
+            throw new Exception("Nieprawidłowa masa");
+        else
+            this.masa = masa;
     }
     public boolean sprawdzNazwe(String nazwa) {
         if (nazwa == null) {
@@ -34,21 +58,21 @@ public class Gwiazda {
             return nazwa.matches("[A-Z]{3}\\d{4}");
         }
     }
-    public boolean sprawdzDeklinacje(String deklinacja, String polkula) {
-        if (polkula.equals("PN")) {
-            return deklinacja.matches("([0-9]|[1-8][0-9]|90) stopni ([0-5][0-9]) minut ([0-5]0-9?) sekund");
-        } else if (polkula.equals("PD")) {
-            return deklinacja.matches("-([0-9]|[1-8][0-9]|90) stopni ([0-5][0-9]) minut ([0-5]0-9?) sekund");
+    public boolean sprawdzDeklinacje(String deklinacja, RodzajPolkuli polkula) {
+        if (polkula==RodzajPolkuli.PN) {
+            return deklinacja.matches("([0-9]|[1-8][0-9]|90) stopni ([0-5][0-9]) minut ([0-5][0-9]?[.][0-5][0-9]?) sekund");
+        } else if (polkula==RodzajPolkuli.PD) {
+            return deklinacja.matches("-([0-9]|[1-8][0-9]|90) stopni ([0-5][0-9]) minut ([0-5][0-9]?[.][0-5][0-9]?) sekund");
         } else {
             return false;
         }
     }
-    public boolean sprawdzPolkule(String polkula) {
+    public boolean sprawdzPolkule(RodzajPolkuli polkula) {
         if (polkula == null) {
             return false;
         }
         else {
-            return polkula.equals("PN") || polkula.equals("PD");
+            return polkula.equals(RodzajPolkuli.PN) || polkula.equals(RodzajPolkuli.PD);
         }
     }
     public boolean sprawdzRektascensje(String rektascensja) {
@@ -56,7 +80,7 @@ public class Gwiazda {
             return false;
         }
         else {
-            return rektascensja.matches("([01][0-9]|2[0-3]|24) h ([0-5][0-9]) m ([0-5]0-9?) s");
+            return rektascensja.matches("([01][0-9]|2[0-3]|24) h ([0-5][0-9]) m ([0-5][0-9]?) s");
         }
     }
     public boolean sprawdzWielkoscGwiazdowa(double wielkoscGwiazdowa) {
@@ -82,13 +106,13 @@ public class Gwiazda {
                 String gwiazdozbior = scanner.nextLine();
                 do {
                     System.out.println("Podaj półkulę gwiazdy (PN/PD): ");
-                    String polkula = scanner.nextLine().toUpperCase();
+                    /*String polkula = scanner.nextLine().toUpperCase();
                     if (sprawdzPolkule(polkula)) {
                         czyWszystkoDobrze = true;
                     } else {
                         czyWszystkoDobrze = false;
                         System.out.println("Nieprawidłowa półkula. Spróbuj ponownie.");
-                    }
+                    }*/
                 } while (!czyWszystkoDobrze);
 
                 do {
@@ -161,6 +185,30 @@ public class Gwiazda {
     }
     public String usunGwiazde(){
         return "";
+    }
+    public double odlegloscWParsekach()
+    {
+        return this.odleglosc*3.2616;
+    }
+    public boolean czySupernowa()
+    {
+        return this.masa>1.44;
+    }
+    public void wyswietl()
+    {
+        System.out.println("=========================================================");
+        System.out.println("Nazwa: "+this.nazwa);
+        System.out.println("Nazwa katalogowa: "+this.nazwaKatalogowa);
+        System.out.println("Deklinacja: "+this.deklinacja);
+        System.out.println("Rektascencja: "+this.rektascensja);
+        System.out.println("Obserwowana wielkosc gwiazdowa: "+this.obserwowanaWielkoscGwiazdowa);
+        System.out.println("Absolutna wielkość gwiazdowa: "+this.absolutnaWielkoscGwiazdowa);
+        System.out.println("Odległość: "+this.odleglosc);
+        System.out.println("Gwiazdozbiór: "+this.gwiazdozbior);
+        System.out.println("półkula:"+this.polkula.toString());
+        System.out.println("Temperatura: "+this.temperatura);
+        System.out.println("Masa: "+this.masa);
+
     }
 }
 
